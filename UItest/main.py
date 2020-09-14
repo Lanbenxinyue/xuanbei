321062adc68f6b939c461041270b34d1f7cb3dee
1073166631@qq.com <Lanbenxinyue>
2020-09-14 17:29:25 +0800

    testtest

--- UItest/main.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-pytest.main(["-s","-v","--html=Outputs/reports/test_report.html","--alluredir=./Outputs/allure_reports"])
-
-#allure serve Outputs/allure_reports   #allure 服务端口执行
-
-#"--reruns" ,"2"  设置失败用例重跑 "--reruns-delay","5" 重跑用例间隔时间
-
